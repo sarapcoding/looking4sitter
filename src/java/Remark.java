9361bbecc056efc0b200c93f05ae59f8db81ee98
URJCheca <alexisasi12@hotmail.com>
2018-02-11 18:15:14 +0100

    Clase Remark añadida

---
+++ src/java/Remark.java
@@ -0,0 +1,25 @@
+package com.dad;
+
+public class Remark {
+ private float puntuacion;
+ private String comentario;
+ private String fecha;
+public float getPuntuacion() {
+	return puntuacion;
+}
+public void setPuntuacion(float puntuacion) {
+	this.puntuacion = puntuacion;
+}
+public String getComentario() {
+	return comentario;
+}
+public void setComentario(String comentario) {
+	this.comentario = comentario;
+}
+public String getFecha() {
+	return fecha;
+}
+public void setFecha(String fecha) {
+	this.fecha = fecha;
+}
+}
