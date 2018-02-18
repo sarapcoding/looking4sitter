6acace1fb62ee78cb214a97aaa0d027e8981ebb5
sarapcoding <sp.nuneza@alumnos.urjc.es>
2018-02-18 11:57:28 +0100

    fix fecha y activo de relusuariosperfiles

--- src/main/java/com/dad/Relusuariosperfiles.java
+++ src/main/java/com/dad/Relusuariosperfiles.java
@@ -1,4 +1,7 @@
 package com.dad;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -14,11 +17,42 @@ public class Relusuariosperfiles {
 	private Long id;
 	private Long idperfil;
 	private Long idusuario;
+	private String fecha;
+	private int activo;
 	
+	public Relusuariosperfiles() {
+		setActivo(1);
+		DateFormat myFecha = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		setFecha(myFecha.format(date));
+	}
 	
-	
-	public Relusuariosperfiles() {}
-	
+
+	public String getFecha() {
+		return fecha;
+	}
+
+
+
+
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+
+
+
+
+	public int getActivo() {
+		return activo;
+	}
+
+
+	public void setActivo(int activo) {
+		this.activo = activo;
+	}
+
 
 	public Long getId() {
 		return id;
