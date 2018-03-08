2863a2d36996f2153f01739beb089d411db488e7
URJCheca <alexisasi12@hotmail.com>
2018-03-08 11:26:08 +0100

    Clases SecurityConfiguration y WebController

---
+++ src/main/java/com/dad/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.dad;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure (HttpSecurity http) throws Exception{
+		//Publico
+		http.authorizeRequests().antMatchers("/").permitAll();
+		http.authorizeRequests().antMatchers("/inicio").permitAll();
+		
+		//Privado
+		http.authorizeRequests().anyRequest().authenticated();
+		
+		//Login
+		http.formLogin().loginPage("/inicio");
+		http.formLogin().usernameParameter("usuario");
+		http.formLogin().passwordParameter("contrasena");
+		http.formLogin().defaultSuccessUrl("/inicio+sesion");
+		http.formLogin().failureUrl("/");
+		
+		
+		// Logout
+		http.logout().logoutUrl("/cerrar-sesion");
+		http.logout().logoutSuccessUrl("/");
+		// CSRF deshabilitado
+		http.csrf().disable();
+	}
+	
+	@Override
+	protected void configure (AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("mia").password("mia123").roles("USER");
+		
+	}
+}
